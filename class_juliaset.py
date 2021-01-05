e79d98cfbb4dec3f652701d6cb05387aca05cbea
peterv748 <peter.verhoeve@caiway.nl>
2021-01-05 17:59:22 +0100

    commit 5 January 2021 001

--- class_juliaset.py
+++ class_juliaset.py
@@ -1,5 +1,5 @@
 """
-    mandelbrot class
+    Julia set
 """
 import numpy as np
 from numba.experimental import jitclass
@@ -10,35 +10,33 @@ spec = [('size_x_axis', int64),('size_y_axis',int64), \
         ('c_real', float64),('c_imag', float64), \
         ('max_iterations',int64)]
 @jitclass(spec)
+class JuliaSet():
+    """
+    JUliaset class
+    """
 
-Class JuliaSet():
-"""
-    mandelbrot class
-"""
+    def __init__(self, image_size, complex_constant, max_iter):
+        """
+        init of class variables
+        """
+        self.size_x_axis = image_size[0]
+        self.size_y_axis = image_size[1]
+        self.c_real = complex_constant[0]
+        self.c_imag = complex_constant[1]
+        self.max_iterations = max_iter
 
-def __init__(self, x_size, y_size, c_real, c_imag, max_iter):
-    """
-    init of class variables
-    """
-    self.size_x_axis = x_size
-    self.size_y_axis = y_size
-    self.c_real = c_real
-    self.c_imag = c_imag
-    self.max_iterations = max_iter
-
-def julia_set(self):
-    """
-    calculate 2D image matrix
-    """
-    
-    h = 2.0/self.size_y_axis
-    for y in range(self.size_y_axis):
-        for x in range(size_x_axis):
-            z_real = -2.0 + x*h
-            z_imag = -1.0 + y*h
-            image[y,x] = 0
-            z_real2 = z_real*z_real
-            z_imag2 = z_imag*z_imag
-            image[y,x] = complex_calculation_juliaset.juliaset_calculate(z_real,\
-                         z_imag, z_real2, z_imag2, c_imag, c_real, maxiter)
-    return image
+    def julia_set_array(self):
+        """
+        calculate 2D image matrix containing the image of the juliaset
+        """
+        image = np.zeros((self.size_y_axis, self.size_x_axis))
+        h = 2.0/self.size_y_axis
+        for y in range(self.size_y_axis):
+            for x in range(self.size_x_axis):
+                z_real = -2.0 + x*h
+                z_imag = -1.0 + y*h
+                z_real2 = z_real*z_real
+                z_imag2 = z_imag*z_imag
+                image[y,x] = complex_calculation_juliaset.juliaset_calculate(z_real,\
+                         z_imag, z_real2, z_imag2, self.c_imag, self.c_real, self.max_iterations)
+        return image
--- julia_set_not_optimized.py
+++ julia_set_not_optimized.py
@@ -14,6 +14,7 @@ def main():
 #initialization of constants
     c_real = -0.835
     c_imag = - 0.2321
+    complex_constant = np.array([-0.835, -0.2321], dtype=np.float64)
     y_size = 2048
     x_size = 2* y_size
     image_box_xmin = -2
@@ -22,12 +23,13 @@ def main():
     image_box_ymax = 1
     image_array = np.zeros((y_size, x_size), dtype=np.uint32)
     max_iterations = np.int64(300)
+
     image_rectangle = np.array([image_box_xmin, image_box_xmax, image_box_ymin, image_box_ymax], \
                                 dtype=np.float64)
     image_size = np.array([x_size,y_size], dtype=np.int64)
-
+    juliaset = class_juliaset.JuliaSet(image_size, complex_constant, max_iterations)
     start = time.time()
-    julia_numba(x_size, y_size, c_real, c_imag, image_array, max_iterations)
+    image_array = juliaset.julia_set_array()
     elapsed_time = time.time() - start
 
     draw_juliaset.plot_juliaset(image_rectangle, image_size, image_array, elapsed_time, max_iterations)
