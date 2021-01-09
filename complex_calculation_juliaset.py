cb88590f6bf064d4d6dee36338950bb76c645518
peterv748 <peter.verhoeve@caiway.nl>
2021-01-09 14:14:25 +0100

    commit 19 January 2021 001

--- complex_calculation_juliaset.py
+++ complex_calculation_juliaset.py
@@ -17,7 +17,27 @@ def juliaset_calculate(cmplx_number_1, cmplx_number_2, cmplx_constant, maxiter):
         cmplx_number_2[1] = cmplx_number_1[1]*cmplx_number_1[1]
         temp = cmplx_number_2[0] + cmplx_number_2[1]
         count = count + 1
-    return count
+    return int(count)
+
+def juliaset_calculate_gpu(z_real_1, z_imag_1, z_real_2, z_imag_2, const_real, const_imag, maxiter):
+    """
+    calculation of julia set formula for gpus
+    """
+    count = 0
+    temp = z_real_2 + z_imag_2
+    temp_real_1 = z_real_1
+    temp_imag_1 = z_imag_1
+    temp_real_2 = z_real_2
+    temp_imag_2 = z_imag_2
+    
+    while (temp <= 4) and (count <= maxiter):
+        temp_imag_1 = 2* temp_real_1*temp_imag_1 + const_imag
+        temp_real_1 = temp_real_2 - temp_imag_2 + const_real
+        temp_real_2 = temp_real_1*temp_real_1
+        temp_imag_2 = temp_imag_1*temp_imag_1
+        temp = temp_real_2 + temp_imag_2
+        count = count + 1
+    return int(count)
 
 jit_module(nopython=True)
 
@@ -35,3 +55,4 @@ if __name__ == "__main__":
 
     print(juliaset_calculate(complex_number_1, complex_number_2, complex_constant, \
                              MAXIMUM_ITERATIONS))
+    
--- julia_set_optimized_cuda_gpu.py
+++ julia_set_optimized_cuda_gpu.py
@@ -4,14 +4,13 @@ Julia Set using the GPU
 
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 from numba import cuda
 import draw_juliaset
 import complex_calculation_juliaset
 
 
 @cuda.jit
-def julia_numba_cuda_kernel(image_size, cpx_number_1, cpx_number_2, cp_constant, image, maxiter):
+def julia_numba_cuda_kernel(image_size, const_real, const_imag, image, maxiter):
     """
     calculation and creation of 2D array with Julia set using GPU
     """
@@ -20,19 +19,23 @@ def julia_numba_cuda_kernel(image_size, cpx_number_1, cpx_number_2, cp_constant,
 
     start_x = cuda.blockDim.x * cuda.blockIdx.x + cuda.threadIdx.x
     start_y = cuda.blockDim.y * cuda.blockIdx.y + cuda.threadIdx.y
-    grid_x = cuda.gridDim.x * cuda.blockDim.x;
-    grid_y = cuda.gridDim.y * cuda.blockDim.y;
-  
+    grid_x = cuda.gridDim.x * cuda.blockDim.x
+    grid_y = cuda.gridDim.y * cuda.blockDim.y
+    z_real_1 = 0.0
+    z_imag_1 = 0.0
+    z_real_2 = 0.0
+    z_imag_2 = 0.0
     
     for j in range(start_y, image_size[1], grid_y):
         for i in range(start_x, image_size[0], grid_x):
-            cpx_number_1[0] = -2.0 + i*stepsize
-            cpx_number_1[1] = -1.0 + j*stepsize
-            cpx_number_2[0] = cpx_number_1[0]*cpx_number_1[0]
-            cpx_number_2[1] = cpx_number_1[1]*cpx_number_1[1]
-            image[j,i] = complex_calculation_juliaset.juliaset_calculate(cpx_number_1, cpx_number_2, \
-                                                                        cp_constant, maxiter)
-            
+            z_real_1 = -2.0 + i*stepsize
+            z_imag_1 = -1.0 + j*stepsize
+            z_real_2 = z_real_1*z_real_1
+            z_imag_2 = z_imag_1*z_imag_1
+            image[j,i] = complex_calculation_juliaset.juliaset_calculate_gpu(z_real_1, \
+                                                    z_imag_1, z_real_2, z_imag_2, \
+                                                    const_real, const_imag, maxiter)
+
 def main():
     """
     main function for calculating and drawing Julia set
@@ -51,18 +54,18 @@ def main():
     image_size = np.array([x_size,y_size], dtype=np.int32)
     image = np.zeros((y_size, x_size), dtype=np.int32)
     image_processed = np.array((y_size, x_size), dtype=np.int32)
-    cpx_number_1 = np.array([0.0, 0.0], dtype=np.float64)
-    cpx_number_2 = np.array([0.0, 0.0], dtype=np.float64)
     
     #start calculation
     d_image = cuda.to_device(image)
     start = time.time()
-    julia_numba_cuda_kernel[grid_dim, block_dim](image_size, cpx_number_1, cpx_number_2, \
-                            complex_constant, d_image, max_iterations)
+    julia_numba_cuda_kernel[grid_dim, block_dim](image_size, \
+                            complex_constant[0], complex_constant[1], \
+                            d_image, max_iterations)
     delta_time = time.time() - start
     image_processed = d_image.copy_to_host()
-    
-    draw_juliaset.plot_juliaset(image_rectangle,image_size, image_processed, delta_time, max_iterations)
+
+    draw_juliaset.plot_juliaset(image_rectangle,image_size, image_processed, \
+                                delta_time, max_iterations)
 
 if __name__ == "__main__":
     main()
